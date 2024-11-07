dc0e4230b654f2d2e63ebaa6b929e18bb8513fcb
medhanies <medhaniesolomon@yahoo.com>
2024-11-06 23:50:15 -0600

    remove dupes

--- removeDuplicates.js
+++ removeDuplicates.js
@@ -2,10 +2,14 @@ function removeDuplicates(numbers) {
   return numbers.reduce(
     (accumulator, currentValue) => {
       // TODO: reduce logic
+      if (accumulator.indexOf(currentValue) === -1) {
+        accumulator.push(currentValue);
+      }
+      return accumulator;
     },
     [] /* TODO: add initial value */
   );
 }
 
-// const numbers = [1, 4, 3, 4, 5, 4, 6, 6, 2];
-console.log(removeDuplicates([1, 4, 3, 4, 5, 4, 6, 6, 2]));
+const numbers = [1, 4, 3, 4, 5, 4, 6, 6, 2];
+console.log(removeDuplicates(numbers));
