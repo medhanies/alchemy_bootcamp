3c3a52f619cae8a6da7fdf4e11f57002fd68d7e3
medhanies <medhaniesolomon@yahoo.com>
2024-11-08 18:16:34 -0600

    fake names

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const myName = "Medhanie";
+// const myName = "Medhanie";
 
-const message = `Hello, ${myName}!`;
+// const message = `Hello, ${myName}!`;
 
+// console.log(message);
+
+// const message = `Hello, ${process.argv}!`;
+
+// console.log(message);
+
+// const message = `Hello, ${process.argv[2]}!`;
+
+// console.log(message);
+
+const { faker } = require("@faker-js/faker");
+const name = faker.person.fullName();
+const message = `Hello, ${name}!`;
 console.log(message);
